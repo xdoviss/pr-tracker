15194d62f8d0302b3f4df461a551b34c14415583
doviss <dovydasp@protonmail.ch>
2023-02-13 12:15:25 +0200

    Added top PR's list and custom modal component

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PrItem from './components/PrItem';
+import { useState } from 'react';
 
 export default function App() {
+  // dummy data
+  const [pr, setPR] = useState([
+    { exercise: 'Bench press', weight: 135, date: '2020-01-01' },
+    { exercise: 'Squat', weight: 225, date: '2020-01-01' },
+    { exercise: 'Deadlift', weight: 315, date: '2020-01-01' },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.headerText}>Personal Records</Text>
+      {pr.map((pr) => (
+        <PrItem key={pr.exercise} pr={pr} />
+      ))}
+
+      <Text style={styles.headerText}>Latest</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -15,6 +29,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    top: 50,
+    //justifyContent: 'center',
+  },
+
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
---
+++ components/PrItem.js
@@ -0,0 +1,63 @@
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import CustomModal from './CustomModal';
+
+const pritem = ({ pr }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <>
+      <CustomModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        pr={pr}
+      />
+
+      <Pressable
+        style={styles.itemCard}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.mainText}>{pr.exercise}</Text>
+        <Text style={styles.subText}>
+          <Text style={{ fontWeight: '500' }}>{pr.weight}</Text> kg.
+        </Text>
+        <Text style={{ fontStyle: 'italic', fontSize: 14 }}>
+          {pr.date}
+        </Text>
+      </Pressable>
+    </>
+  );
+};
+
+export default pritem;
+
+const styles = StyleSheet.create({
+  itemCard: {
+    backgroundColor: '#fff',
+    width: '90%',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+
+  mainText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+  subText: {
+    position: 'absolute',
+    right: 20,
+    top: 20,
+
+    fontSize: 18,
+    fontStyle: 'italic',
+  },
+});
